7f3d1c788ab7f870d1698b0cd2a93b9d6356f6d1
Oleksandr Dzisiak <shumahersasha007@gmail.com>
2023-02-20 13:49:22 +0100

    add html

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      width: {
+        DEFAULT: "100%",
+        phone: "320pxm",
+        tablet: "760px",
+        laptop: "1650px",
+      },
+    },
+
+    screens: {
+      phone: "360px",
+      teblet: "820px",
+      laptop: "1920px",
+    },
+
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      dark: "#171717",
+      main: "#FF304D",
+      activeRed: "#DC2D45",
+      lightred: "#FF445E",
+      grey: "#323232",
+      darkgrey: "#474747",
+      middlegrey: "#919191",
+      lightgrey: "#E0E0E0",
+      light: "#F8F8F8",
+      white: "#FFFFFF",
+      prototype: "#457292",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
